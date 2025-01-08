17cde34a860926d9b9313459e05cbfaa55d37353
Mathieu <mathieu.boucher@epsi.fr>
2025-01-08 15:03:44 +0100

    add songs service with some methods to post a new song and fetch all songs

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,20 +1,25 @@
 import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { SongsService } from './songs.service';
 
 @Controller('songs')
 export class SongsController {
+  constructor(private songsService: SongsService) {}
+
+  @Post()
+  create() {
+    // save the song in the db
+    return this.songsService.create('Animals by Martin Garrix');
+  }
+
   @Get()
-  findAll(): string {
-    return 'Find all songs';
+  findAll() {
+    // fetch all songs from the db
+    return this.songsService.findAll();
   }
 
   @Get(':id')
   findOne(): string {
     return 'Fetch song on the based id';
-  }
-
-  @Post()
-  create(): string {
-    return 'Create a new song';
   }
 
   @Put(':id')
---
+++ src/songs/songs.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class SongsService {
+  // local db
+
+  private readonly songs = [];
+
+  create(song) {
+    this.songs.push(song);
+    return this.songs;
+  }
+
+  findAll() {
+    return this.songs;
+  }
+}
